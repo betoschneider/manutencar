2bd52b6a262bd8c646e1c48d35ea8867a0e3c7d8
Beto Schneider <roberto.schneider@gmail.com>
2025-12-28 11:22:15 -0300

    fix frontend and docker-compose

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Carrega variáveis de ambiente (sem exigir prefixo VITE_)
   const env = loadEnv(mode, process.cwd(), '')
 
-  // Permite múltiplos hosts separados por vírgula no .env
   const allowedHosts = env.ALLOWED_HOSTS
     ? env.ALLOWED_HOSTS.split(',').map(h => h.trim())
     : []
@@ -14,17 +11,16 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
 
-    // Vite DEV
     server: {
-      host: true, // 0.0.0.0
+      host: true,
+      port: 5173,
       allowedHosts,
     },
 
-    // Vite PREVIEW (produção via npm run preview)
     preview: {
-      host: true, // 0.0.0.0
-      allowedHosts,
+      host: true,
       port: 5173,
+      allowedHosts,
     },
   }
 })
